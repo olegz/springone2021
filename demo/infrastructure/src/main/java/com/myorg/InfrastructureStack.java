c412b6cc9dd18d60b51595ea2d1c6916a77a1d7c
msailes <msailes@amazon.co.uk>
2021-08-10 16:13:11 +0100

    The initial almost working version.

---
+++ demo/infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -0,0 +1,88 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.BundlingOptions;
+import software.amazon.awscdk.core.CfnOutput;
+import software.amazon.awscdk.core.CfnOutputProps;
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.DockerImage;
+import software.amazon.awscdk.core.DockerVolume;
+import software.amazon.awscdk.core.Duration;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.apigatewayv2.AddRoutesOptions;
+import software.amazon.awscdk.services.apigatewayv2.HttpApi;
+import software.amazon.awscdk.services.apigatewayv2.HttpApiProps;
+import software.amazon.awscdk.services.apigatewayv2.HttpMethod;
+import software.amazon.awscdk.services.apigatewayv2.PayloadFormatVersion;
+import software.amazon.awscdk.services.apigatewayv2.integrations.LambdaProxyIntegration;
+import software.amazon.awscdk.services.apigatewayv2.integrations.LambdaProxyIntegrationProps;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.RetentionDays;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static software.amazon.awscdk.core.BundlingOutput.ARCHIVED;
+
+public class InfrastructureStack extends Stack {
+    public InfrastructureStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        List<String> functionOnePackagingInstructions = Arrays.asList(
+                "-c",
+                "cd uppercase " +
+                "&& mvn clean package -P native,native-image -DskipTests " +
+                "&& cp /asset-input/uppercase/target/cloud-function-aws-0.0.1-SNAPSHOT-native-zip.zip /asset-output/"
+        );
+
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(functionOnePackagingInstructions)
+                .image(DockerImage.fromRegistry("marksailes/al2-graalvm"))
+                .volumes(singletonList(
+                        DockerVolume.builder()
+                                .hostPath(System.getProperty("user.home") + "/.m2/")
+                                .containerPath("/root/.m2/")
+                                .build()
+                ))
+                .user("root")
+                .outputType(ARCHIVED);
+
+        Function uppercaseFunction = new Function(this, "UppercaseFunction", FunctionProps.builder()
+                .runtime(Runtime.PROVIDED_AL2)
+                .code(Code.fromAsset("../software/", AssetOptions.builder()
+                        .bundling(builderOptions.build())
+                        .build()))
+                .handler("foobar")
+                .timeout(Duration.seconds(30))
+                .memorySize(256)
+                .logRetention(RetentionDays.ONE_WEEK)
+                .build());
+
+        HttpApi httpApi = new HttpApi(this, "SpringOneAPI", HttpApiProps.builder()
+                .apiName("SpringOneAPI")
+                .build());
+
+        httpApi.addRoutes(AddRoutesOptions.builder()
+                .path("/uppercase")
+                .methods(singletonList(HttpMethod.GET))
+                .integration(new LambdaProxyIntegration(LambdaProxyIntegrationProps.builder()
+                        .handler(uppercaseFunction)
+                        .payloadFormatVersion(PayloadFormatVersion.VERSION_2_0)
+                        .build()))
+                .build());
+
+        CfnOutput apiUrl = new CfnOutput(this, "UppercaseApiUrl", CfnOutputProps.builder()
+                .exportName("UppercaseApiUrl")
+                .value(httpApi.getApiEndpoint())
+                .build());
+    }
+}
---
+++ demo/software/uppercase/src/shell/native/bootstrap
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd ${LAMBDA_TASK_ROOT:-.}
+
+./cloud-function-aws -Dlogging.level.org.springframework=DEBUG
