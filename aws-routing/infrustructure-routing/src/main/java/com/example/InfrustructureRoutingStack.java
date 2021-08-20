53ce1a2fc27c67962016b9c9f88e557c58597428
Mark Sailes <msailes@amazon.co.uk>
2021-08-20 16:04:03 +0100

    Try this

--- aws-routing/infrustructure-routing/src/main/java/com/example/InfrustructureRoutingStack.java
+++ aws-routing/infrustructure-routing/src/main/java/com/example/InfrustructureRoutingStack.java
@@ -1,13 +1,15 @@
 package com.example;
 
-
-
 import static java.util.Collections.singletonList;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import software.amazon.awscdk.core.BundlingOptions;
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.DockerVolume;
 import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
@@ -22,6 +24,7 @@ import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.FunctionProps;
 import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
 
 public class InfrustructureRoutingStack extends Stack {
     public InfrustructureRoutingStack(final Construct scope, final String id) {
@@ -32,16 +35,31 @@ public class InfrustructureRoutingStack extends Stack {
         super(scope, id, props);
 
         Map<String, String> rngEnvVars = new HashMap<>();
-        rngEnvVars.put("LOG_LEVEL", "INFO");
-        rngEnvVars.put("POWERTOOLS_SERVICE_NAME", "User-Service");
+        rngEnvVars.put("spring_cloud_function_definition", "function");
 
-//        rngEnvVars.put("spring_cloud_function_definition", "function");
+        List<String> functionOnePackagingInstructions = Arrays.asList(
+                "/bin/sh",
+                "-c",
+                "cd function-aws-springone ",
+                "&& mvn clean install ",
+                "&& cp /asset-input/function-aws-springone/target/function-aws-springone-1.0.0.RELEASE-aws.jar /asset-output/");
 
+        BundlingOptions.Builder builderOptions = BundlingOptions.builder()
+                .command(functionOnePackagingInstructions)
+                .image(Runtime.JAVA_11.getBundlingImage())
+                .volumes(singletonList(DockerVolume.builder()
+                        .hostPath(System.getProperty("user.home") + "/.m2/")
+                        .containerPath("/root/.m2/")
+                        .build()))
+                .user("root");
 
-        Function rngService = new Function(this, "User-Service", FunctionProps.builder()
+        Function rngService = new Function(this, "FunctionRouter", FunctionProps.builder()
                 .runtime(Runtime.JAVA_11)
                 .functionName("functionRouter")
-                .code(Code.fromAsset("../function-aws-springone/target/function-aws-springone-1.0.0.RELEASE-aws.jar"))
+                .code(Code.fromAsset("../function-aws-springone/", AssetOptions.builder()
+                        .bundling(builderOptions
+                                .build())
+                        .build()))
                 .handler("org.springframework.cloud.function.adapter.aws.FunctionInvoker")
                 .memorySize(1024)
                 .timeout(Duration.seconds(20))
